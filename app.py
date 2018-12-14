3ae67ba5db21f2f351098e0b9cb1f4e172bfc5eb
gacky35 <s17t258@stu.kagawa-u.ac.jp>
2018-12-14 22:35:32 +0900

    いろいろした

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 import psycopg2
 
 app = Flask(__name__)
+app.secret_key = 'hogehugapiyoham'
 connection = psycopg2.connect("host=localhost dbname=todo user=postgres password=postgres")
 cur = connection.cursor()
-session = {}
+
 
 @app.route('/')
 def log_in():
     return render_template('index.html')
 
 @app.route('/add.html')
-def add():
+def adding():
     return render_template('add.html')
 
 @app.route('/add.html',methods=['POST'])
-def adding():
+def add():
     if request.method == 'POST':
         cur.execute("SELECT COUNT(id) FROM task")
         rows = cur.fetchall()
@@ -28,32 +29,29 @@ def adding():
 
 @app.route('/list.html', methods=['POST'])
 def display():
-    global session
     if request.method == 'POST':
-        username = request.form['user_name']
-        session[0] = username
-        cur.execute("SELECT password FROM users WHERE username='" + username + "'")
+        session['name'] = request.form['user_name']
+        password = request.form['password']
+        username = session.get('name')
+        cur.execute("SELECT 1 FROM users WHERE username='" + username + "' AND password=md5('" + password + "')")
         results = cur.fetchall()
         if not results:
             return render_template('index.html')
-        for row in results:
-            result = row[0]
-        if result == request.form['password']:
+        else:
             tasks = pick()
             return render_template('list.html', username=username, tasks=tasks)
-        else:
-            return render_template('index.html')
 
 @app.route('/edit.html', methods=['POST'])
 def edited():
     if request.method == 'POST':
-        cur.execute("UPDATE task SET content = '" + request.form['contents'] + "' WHERE id = '" + session[1] + "'")
-        cur.execute("UPDATE task SET status = '" + request.form['status'] + "' WHERE id = '" + session[1] + "'")
-        cur.execute("UPDATE task SET deadline = '" + request.form['deadline'] + "' WHERE id = '" + session[1] + "'")
-        cur.execute("UPDATE task SET detail = '" + request.form['detail'] + "' WHERE id = '" + session[1] + "'")
+        no = session.get('no')
+        cur.execute("UPDATE task SET content = '" + request.form['contents'] + "' WHERE id = '" + no + "'")
+        cur.execute("UPDATE task SET status = '" + request.form['status'] + "' WHERE id = '" + no + "'")
+        cur.execute("UPDATE task SET deadline = '" + request.form['deadline'] + "' WHERE id = '" + no + "'")
+        cur.execute("UPDATE task SET detail = '" + request.form['detail'] + "' WHERE id = '" + no + "'")
         connection.commit()
     tasks = pick()
-    return render_template('list.html', username=session[0], tasks=tasks)
+    return render_template('list.html', username=session.get('name'), tasks=tasks)
 
 @app.route('/regist.html')
 def regist_disp():
@@ -61,19 +59,19 @@ def regist_disp():
 
 @app.route('/index.html')
 def log_out():
-    global session
-    session = {}
+    session.pop('name', None)
     return render_template('index.html')
 
 @app.route('/index.html', methods = ['POST'])
 def regist():
     if request.method == 'POST':
         username = request.form['user_name']
+        password = request.form['password']
         cur.execute("SELECT * FROM users WHERE username='" + username + "'")
         check = cur.fetchall()
         if not check:
-            if ( request.form['password'] == request.form['confirm'] ):
-                cur.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (username, request.form['password']))
+            if ( password == request.form['confirm'] ):
+                cur.execute("INSERT INTO users (username, password) VALUES ('" + username + "', md5('" + password + "'))")
                 connection.commit()
                 return render_template('index.html')
             else:
@@ -82,21 +80,18 @@ def regist():
 
     return render_template('regist.html')
 
-@app.route('/<get_id>')
+@app.route('/<int:get_id>')
 def edit(get_id):
-    global session
-    if get_id == 'favicon.ico':
-        pass
-    else:
-        session[1] = get_id
-        cur.execute("select * from task where id = '" + get_id + "'")
-        tasks = cur.fetchall()
-        return render_template('edit.html', tasks=tasks)
+    session['no'] = str(get_id)
+    cur.execute("select * from task where id = '" + str(get_id) + "'")
+    tasks = cur.fetchall()
+    return render_template('edit.html', tasks=tasks)
 
 def pick():
-    cur.execute("SELECT * FROM task WHERE username='" + session[0] + "'")
+    username = session.get('name')
+    cur.execute("SELECT * FROM task WHERE username='" + username + "'")
     tasks = cur.fetchall()
     return tasks
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=53487)
